30cabaa9f7370f8c9fb4f0b19f4f298316b95acf
Thomas Goodwin <btgoodwin@geontech.com>
2014-10-02 09:51:57 -0400

    Initial commit of STDIN/STDOUT wrapper example

--- hawkeye/util/rh_gateway/wrappers/std_front.py
+++ hawkeye/util/rh_gateway/wrappers/std_front.py
@@ -23,5 +23,66 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import RH_Gateway
 
-if __name__ == "__main__":
-    print "Hello World"
+# `sys` for flushing stdout
+# `json` for converting the RH_Message list between strings/object format.
+# `gevent, signal, fcntl, os` all related to the read/write through the gateway.
+import sys
+import json
+import gevent
+from gevent.socket import wait_read
+import signal
+import fcntl
+import os
+import logging
+
+"""
+This worker attempts to readline() from STDIN and pass any resulting messages
+to the inbox.  This has been adapted from: https://gist.github.com/tmc/787105
+"""
+def inboundWorker(callback):
+    fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
+    while True:
+        wait_read(sys.stdin.fileno())
+        line = sys.stdin.readline()
+        try:
+            messages = json.loads(line)
+            callback(messages) # Into the RH_Gateway
+        except:
+            logging.warning("Dropped bad JSON string from input")
+        gevent.sleep(0)
+
+"""
+This worker listens to the outbox and forwards anything found to STDOUT
+"""
+def outboundWorker(callback):
+    while True:
+        messages = callback()
+        if (0 < len(messages)):
+            print(json.dumps(messages)); sys.stdout.flush();
+        gevent.sleep(0)
+
+"""
+Spawn greenlets and wait (joinall)
+"""
+if __name__ == '__main__':
+    logging.getLogger('std_front').setLevel(logging.INFO)
+    try:
+        gw = RH_Gateway()
+        gw.start()
+        ge_inbox = gevent.spawn(inboundWorker, ge_gateway.sendMessages)
+        ge_outbox = gevent.spawn(outboundWorker, ge_gateway.getMessages)
+
+        # Capture interruptions/exits
+        gevent.signal(signal.SIGTERM, ge_inbox.kill)
+        gevent.signal(signal.SIGINT, ge_inbox.kill)
+        gevent.signal(signal.SIGTERM, ge_outbox.kill)
+        gevent.signal(signal.SIGINT, ge_outbox.kill)
+        
+        # Wait...
+        gevent.joinall([ge_inbox, ge_outbox])
+    except:
+        raise
+    finally:
+        # Clean-up
+        gw.stop()
+    
